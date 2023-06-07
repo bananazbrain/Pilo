3dab40fd07db53dc74df76363416d046c53ca2cc
Julia <jv@bananazbrain.ru>
2023-06-07 15:05:35 +0300

    quiz step 1

--- source/dev/_/sys/js/common.js
+++ source/dev/_/sys/js/common.js
@@ -28,6 +28,75 @@ window.onload = () => {
     })
   }
 
+  // CHECK
+  class Check {
+    static addError(element, message) {
+      element.classList.add(Check.classError);
+
+      if (message) {
+        element.querySelector('.check__message').innerText = message;
+      }
+    }
+
+    constructor(element) {
+      this.element = element;
+      this.input = this.element.input = this.element.querySelector('.check__input');
+      this.type = this.input.type;
+      this.checkChecked();
+      this.onClick();
+
+      this.element.input = this.input;
+    }
+
+    onClick() {
+      this.element.addEventListener('click', (event) => {
+        this.element.classList.remove('--error');
+        if (event.target.closest('[data-popup]') || this.element.classList.contains('--disabled')) {
+          return false;
+        }
+        if (this.type == 'checkbox') {
+          this.element.classList.toggle('--checked');
+          this.checked = this.input.getAttribute('checked');
+
+          if (this.checked) {
+            this.input.removeAttribute('checked');
+          } else {
+            this.input.setAttribute('checked', 'checked');
+          }
+
+        } else if (this.type == 'radio') {
+          if (this.element.closest('--checked')) {
+            return false;
+          }
+
+          this.name = this.input.name;
+          let parent = this.element.closest('.checks') ? this.element.closest('.checks') : this.element.closest('form') ? this.element.closest('form') : document.body;
+          let radios = parent.querySelectorAll('.check input[type="radio"][name="' + this.name + '"]');
+          radios.forEach(radio => {
+            radio.removeAttribute('checked');
+            radio.closest('.check').classList.remove('--checked');
+          });
+          this.element.classList.add('--checked');
+          this.input.setAttribute('checked', 'checked');
+        }
+      });
+    }
+
+    checkChecked() {
+      if (this.input.getAttribute('checked')) {
+        this.element.classList.add('--checked');
+      }
+    }
+  }
+
+  // CHECK INIT
+  let checks = document.querySelectorAll('.check');
+  if (checks) {
+    checks.forEach((check) => {
+      new Check(check);
+    });
+  }
+
   // FANCYBOX SETUP
   Fancybox.bind("[data-fancybox]", {
     defaultDisplay: 'flex',
